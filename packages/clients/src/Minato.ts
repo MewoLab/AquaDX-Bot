e3df13ed7c636946f3f655850b602566fe2a0171
Clansty <i@gao4.pw>
2025-04-13 04:31:29 +0800

    fix: minato b50

--- packages/clients/src/Minato.ts
+++ packages/clients/src/Minato.ts
@@ -2,6 +2,6 @@ import AquaDxLike from './AquaDxLike';
 
 export default class Minato extends AquaDxLike {
 	public constructor() {
-		super('http://11.11.41.11');
+		super('https://minato-web-api.init.ink');
 	}
 }
